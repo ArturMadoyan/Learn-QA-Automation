d7625f1763092b987381f1de9cd858b059a63ee5
Artur <madoyan.artur@gmail.com>
2021-05-12 22:16:01 +0400

    AAAAA

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Test2 {
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("MAdoyan2");
+        System.out.println("Madoyan3");
+    }
 }
